2e95604afd461fb63051b11b0b639356cb19cb1e
logonrm <logonrm@fiap.com.br>
2018-02-17 10:16:00 -0200

    Incluido o Boot Broadcast que utiliza o Toast quando o celular liga

---
+++ app/src/main/java/com/satoru/kishi/demoorderedbroadcast/BootBroadcastReceiver.kt
@@ -0,0 +1,16 @@
+package com.satoru.kishi.demoorderedbroadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+/**
+ * Created by logonrm on 17/02/2018.
+ */
+class BootBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, "Terminei a inicialização", Toast.LENGTH_SHORT).show()
+    }
+
+}
