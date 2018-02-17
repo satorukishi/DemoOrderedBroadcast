a93cb5a68005bc43cdf7107f7b77bef2b6514d2a
logonrm <logonrm@fiap.com.br>
2018-02-17 10:46:06 -0200

    Receiver de tomada

--- app/src/main/java/com/satoru/kishi/demoorderedbroadcast/MainActivity.kt
+++ app/src/main/java/com/satoru/kishi/demoorderedbroadcast/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.satoru.kishi.demoorderedbroadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var receiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when (intent?.action) {
+                    Intent.ACTION_POWER_CONNECTED -> tvUSBStatus.text = "Cabo conectado"
+                    Intent.ACTION_POWER_DISCONNECTED -> {
+                        tvUSBStatus.text = "Cabo desconectado"
+                    }
+                }
+            }
+        }
+        registerReceiver(receiver, filter)
+
         // É possível utilizar Lambda expression aqui porque a interface OnClickListener só tem um método
         btEnviar.setOnClickListener {
             sendBroadcast(Intent("com.satoru.kishi.demoorderedbroadcast.MeuOrderedBroadcast"))
-            sendBroadcast(Intent("com.satoru.kishi.demoorderedbroadcast.MeuOutroBroadcast"))
         }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
+
+
     }
 }
