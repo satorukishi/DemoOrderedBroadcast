579c2fe0dd4896ec3d1553fa84a56ac748f39a8d
logonrm <logonrm@fiap.com.br>
2018-02-17 09:57:28 -0200

    Adicionado o Broadcast Receiver

--- app/src/main/java/com/satoru/kishi/demoorderedbroadcast/MainActivity.kt
+++ app/src/main/java/com/satoru/kishi/demoorderedbroadcast/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.satoru.kishi.demoorderedbroadcast
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // É possível utilizar Lambda expression aqui porque a interface OnClickListener só tem um método
+        btEnviar.setOnClickListener {
+            sendBroadcast(Intent("com.satoru.kishi.demoorderedbroadcast.MeuOrderedBroadcast"))
+            sendBroadcast(Intent("com.satoru.kishi.demoorderedbroadcast.MeuOutroBroadcast"))
+        }
     }
 }
